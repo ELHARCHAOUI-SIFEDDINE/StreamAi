a0d11309713a747782bc069ca8724f7b4f274f45
ELHARCHAOUI-SIFEDDINE <sifddinharchaoui2003@gmail.com>
2024-11-05 09:31:36 +0100

    Add Auth context firebase

---
+++ src/context/AuthContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../utils/firebaseconfig";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user); // Set user state to the currently logged-in user
+    });
+
+    return () => unsubscribe(); // Cleanup subscription on unmount
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
